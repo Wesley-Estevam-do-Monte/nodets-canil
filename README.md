2701f1a39876b10300c30169b5fadf394cb99f84
Wesley <wesleyestevam15@gmail.com>
2021-12-25 16:34:40 -0300

    Servidor e rotas criadas

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # nodets-canil
+Projeto de treino para o aprendizado do node.js com Typescript.
+
+### Pré-requisitos globais: 
+`npm install -g nodemon typescript ts-node`
+
+### Instalação 
+`npm install`
+
+### Para rodar o projeto 
+`npm rum start-dev`
---
+++ src/controllers/searchController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+
+export const home = ( req: Request, res: Response ) => {
+
+   // res.render('pages/search')
+}
+
+export const search = ( req: Request, res: Response ) => {
+
+    // res.render('pages/search')
+ }
+ 
